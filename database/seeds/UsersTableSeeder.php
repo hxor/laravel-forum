2a91b972707936a742f5d986de6adb9c4f496160
hxor <hakimsembiring@outlook.co.id>
2018-07-12 09:54:19 +0700

    Add Admin Table Seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\User::create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'password' => bcrypt('password'),
+            'admin' => 1,
+            'avatar' => asset('/image/avatar/user_admin')
+        ]);
+    }
+}
